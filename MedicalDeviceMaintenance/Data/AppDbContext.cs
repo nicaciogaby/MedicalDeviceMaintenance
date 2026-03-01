64412ae5c9ab132f58604efca75dd0228a3fcfc1
Gabriela Nicacio Lima <S00274684@atu.ie>
2026-03-01 05:54:53 +0000

    add EF Core DbContext and SQL Server connection

---
+++ MedicalDeviceMaintenance/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MedicalDeviceMaintenance.Models;
+
+namespace MedicalDeviceMaintenance.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Device> Devices => Set<Device>();
+        public DbSet<Incident> Incidents => Set<Incident>();
+        public DbSet<MaintenanceAction> MaintenanceActions => Set<MaintenanceAction>();
+    }
+}
--- MedicalDeviceMaintenance/Program.cs
+++ MedicalDeviceMaintenance/Program.cs
@@ -1,3 +1,6 @@
+using MedicalDeviceMaintenance.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace MedicalDeviceMaintenance
 {
     public class Program
@@ -8,7 +11,8 @@ namespace MedicalDeviceMaintenance
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
-
+            builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
