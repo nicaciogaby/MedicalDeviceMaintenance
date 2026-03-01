9f3b8b6f3dfc0a8877aee2f7d24e9f344851b7ef
Gabriela Nicacio Lima <S00274684@atu.ie>
2026-03-01 05:28:29 +0000

    Initial ASP.NET Core MVC project setup

---
+++ MedicalDeviceMaintenance/Models/Device.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MedicalDeviceMaintenance.Models
+{
+    public class Device
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public string? SerialNumber { get; set; }
+        public string? Location { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
