2a3ffc8f295bc831f85356205eddfcd098ac908d
Gabriela Nicacio Lima <S00274684@atu.ie>
2026-03-01 05:40:27 +0000

    add Incifeat: add MaintenanceAction modeldent model

---
+++ MedicalDeviceMaintenance/Models/Incident.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MedicalDeviceMaintenance.Models
+{
+    public enum IncidentStatus
+    {
+        Open = 0,
+        InProgress = 1,
+        Closed = 2
+    }
+
+    public class Incident
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public IncidentStatus Status { get; set; } = IncidentStatus.Open;
+
+        public DateTime ReportedAt { get; set; } = DateTime.UtcNow;
+
+        
+        public int? DeviceId { get; set; }
+        public Device? Device { get; set; }
+    }
+}
---
+++ MedicalDeviceMaintenance/Models/MaintenanceAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MedicalDeviceMaintenance.Models
+{
+    public class MaintenanceAction
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string ActionTaken { get; set; } = string.Empty;
+
+        public DateTime ActionDate { get; set; } = DateTime.UtcNow;
+
+        // Link to Incident
+        public int IncidentId { get; set; }
+        public Incident? Incident { get; set; }
+    }
+}
