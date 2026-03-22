daa6070ec98d9396b62dac699a639689cd9a2507
Gabriela Nicacio Lima <S00274684@atu.ie>
2026-03-22 09:17:11 +0000

    feat: implement Incident CRUD and relationship with Device

--- MedicalDeviceMaintenance/Controllers/IncidentsController.cs
+++ MedicalDeviceMaintenance/Controllers/IncidentsController.cs
@@ -54,8 +54,7 @@ namespace MedicalDeviceMaintenance.Controllers
         }
 
         // POST: Incidents/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Title,Description,Status,ReportedAt,DeviceId")] Incident incident)
